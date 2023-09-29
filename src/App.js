e7b24f253e5cfb5d9c16a1a187df80f673ae820d
GmanSibs1906 <gmansibs@gmail.com>
2023-09-29 11:38:31 +0200

    navbar design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React from 'react';
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './pages/Home';
+import Navbar from './componants/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/assets/images/index.js
@@ -0,0 +1,25 @@
+// Logo
+import logo from "./apple-logo.png";
+
+// home screen
+import home14Banner from "./i4ProBanner.jpg";
+import homeIphone14 from "./iphone-14.png";
+import homeIpad from "./ipad_home.png";
+import homeIwatch from "./iwatch_8.jpg";
+import homeM2 from "./macbookM2.png";
+import homeStudio from "./studio.png";
+import homeWatchDeal from "./watch_deal.webp";
+
+export {
+    // logo
+    logo,
+
+    // home screen
+    home14Banner,
+    homeIpad,
+    homeIphone14,
+    homeIwatch,
+    homeM2,
+    homeStudio,
+    homeWatchDeal
+}
