7bd8c6402ee54881c61b72fa80b3c16b72ce2685
GmanSibs1906 <gmansibs@gmail.com>
2023-10-04 17:09:20 +0200

    mobile

--- src/App.js
+++ src/App.js
@@ -12,8 +12,17 @@ import Product from './pages/Product';
 import Cart from './pages/Cart';
 import CheckOut from './pages/CheckOut';
 import Orders from './pages/Orders';
+import MobileMenu from './componants/navbar/MobileMenu';
 
 function App() {
+
+
+    const [isOverlayVisible, setIsOverlayVisible] = useState(false);
+  
+    const toggleOverlay = () => {
+      setIsOverlayVisible(!isOverlayVisible);
+    };
+
   return (
     <Router>
       <Navbar />
@@ -25,6 +34,16 @@ function App() {
         <Route path="/checkout" element={<CheckOut />} />
         <Route path="/orders" element={<Orders />} />
       </Routes>
+
+       {/* Add the Overlay component with conditional rendering */}
+       {isOverlayVisible && <MobileMenu onClose={toggleOverlay} />}
+
+{/* Button to toggle the overlay */}
+<button onClick={toggleOverlay} className="fixed bottom-4 right-4 p-2 bg-blue-500 text-white rounded-full hover:bg-blue-700 cursor-pointer">
+  Show Overlay
+</button>
+
+
     </Router>
   );
 }
--- src/componants/navbar/MobileMenu.js
+++ src/componants/navbar/MobileMenu.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
-function MobileMenu() {
+function MobileMenu({ onClose }) {
   return (
-    <div className=' flex flex-col w-full h-[96vh] mt-[50px] md:hidden bg-red-500 '>MobileMenu</div>
+    <div className=" md:hidden fixed inset-0 flex items-center justify-center z-50">
+      <div className="fixed inset-0 bg-gray-700 opacity-70"></div>
+      <div className="relative z-10 bg-white rounded-lg p-4">
+        {/* Your content here */}
+        <p>Hello, I'm the overlay!</p>
+        <button
+          onClick={onClose}
+          className="absolute top-0 right-0 m-2 text-gray-600 hover:text-gray-900 cursor-pointer"
+        >
+          Close
+        </button>
+      </div>
+    </div>
   )
 }
 
