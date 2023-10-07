07747f3183b77c87cbbb2da2be0c738b82ec4229
Gman1906Sibs <77187437+Gman1906Sibs@users.noreply.github.com>
2023-10-07 19:29:06 +0200

    dashboard set up

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App text-red-500 underline font-bolt text-3xl">
+      DASHBOARD
+    </div>
+  );
+}
+
+export default App;
